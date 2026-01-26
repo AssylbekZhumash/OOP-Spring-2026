4b21df1fa3fc04778906a38b86c475d21b6e8e02
ZhumashAssylbek <asyylbekjumashh@gmail.com>
2026-01-26 23:54:02 +0500

    Commit

---
+++ src/pr1/tsk5.java
@@ -0,0 +1,18 @@
+package pr1;
+import java.util.Scanner;
+public class tsk5 {
+    public static void main(String[] args) {
+
+        int balance = 0;
+        System.out.println("Your current balance is: " + balance);
+        System.out.println("Please enter your money: ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int money = scanner.nextInt();
+        balance += money;
+
+        System.out.println("You inserted " + money + "KZT! Your current balance is " + balance);
+
+    }
+}
