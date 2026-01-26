616e4fbfbcc6fe6fa174392ea35ecf9e6f065996
ZhumashAssylbek <asyylbekjumashh@gmail.com>
2026-01-26 21:37:25 +0500

    Commit

---
+++ src/pr1/tsk2.java
@@ -0,0 +1,17 @@
+package pr1;
+import java.util.Scanner;
+
+public class tsk2 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Length of one side?");
+        int a = scanner.nextInt();
+        double b = a;
+
+        System.out.println("Area:" + a*a);
+        System.out.println("Perimeter" + 4*a);
+        System.out.println("Diagonal" + Math.sqrt(2)*b);
+    }
+
+}
